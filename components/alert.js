812b1531ebf4dafe92b082a4897179ae2b3e9236
datracka <datracka@gmail.com>
2020-05-09 09:19:39 +0200

    change title and quote

--- components/alert.js
+++ components/alert.js
@@ -1,16 +1,16 @@
-import Container from './container'
-import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
+import Container from "./container";
+import cn from "classnames";
+import { EXAMPLE_PATH } from "../lib/constants";
 
 export default function Alert({ preview }) {
   return (
     <div
-      className={cn('border-b', {
-        'bg-accent-7 border-accent-7 text-white': preview,
-        'bg-accent-1 border-accent-2': !preview,
+      className={cn("border-b", {
+        "bg-accent-7 border-accent-7 text-white": preview,
+        "bg-accent-1 border-accent-2": !preview,
       })}
     >
-      <Container>
+      {/* <Container>
         <div className="py-2 text-center text-sm">
           {preview ? (
             <>
@@ -36,7 +36,7 @@ export default function Alert({ preview }) {
             </>
           )}
         </div>
-      </Container>
+      </Container> */}
     </div>
-  )
+  );
 }
